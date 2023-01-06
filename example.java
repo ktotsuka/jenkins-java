fb1241ec6ef1ab695758b98da1a30668076e149b
Kenji Totsuka <kenji.totsuka@gmail.com>
2023-01-06 14:25:17 -0500

    misspell

--- example.java
+++ example.java
@@ -6,6 +6,6 @@
 class Example { 
     // A Java program begins with a call to main(). 
     public static void main(String[] args) { 
-      System.out.println("Java drives the Web."); 
+      System.out.prntln("Java drives the Web."); 
     } 
 }
